4a12389e940c2249e51376319de3994efdc512f6
Sameh <same7.uwk007@gmail.com>
2025-08-08 06:14:44 +0300

    add sorting functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import text_word_count
 from stats import char_nth_times
+from stats import sorted_chars
 
 
 def get_book_text(file_path):
@@ -8,13 +9,13 @@ def get_book_text(file_path):
         return file_content
 
 BOOK_PATH = "./books/frankenstein.txt"
-frankenstein_book_text = get_book_text(BOOK_PATH)
 
 
 
 def main():
-    text_word_count(get_book_text(BOOK_PATH))
+    frankenstein_book_text = get_book_text(BOOK_PATH)
+    text_word_count(frankenstein_book_text)
     char_nth_times(frankenstein_book_text)
-
+    sorted_chars(char_nth_times(frankenstein_book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -11,9 +11,29 @@ def char_nth_times(text):
         lower_char = char.lower()
         char_dict[lower_char] = char_dict.get(lower_char, 0) + 1 
     
-    print(char_dict)
+    # print(char_dict)
     return char_dict
 
+def sort_on(item):
+    return item["num"]
+
+
+
+def sorted_chars(chars_dict):
+    chars_list = []
+
+    for char in chars_dict:
+        if char.isalpha():
+            char_count = chars_dict[char]
+            chars_list.append({
+                "char": char,
+                "num": char_count
+            })
+    
+    chars_list.sort(reverse=True, key=sort_on)
+    print(chars_list)
+    return chars_list
+    
 """
 create a function that will return the number of times a char shown in text including special chars
 use .lower()
